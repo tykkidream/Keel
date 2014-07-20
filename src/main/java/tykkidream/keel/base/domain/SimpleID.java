ed4b7350c33428a8a447f2b87482f23f506238c5
Tykkidream <Tykkidream@gmail.com>
2014-07-20 15:48:07 +0800

    补充SimpleID中id的getter和setter。

--- src/main/java/tykkidream/keel/base/domain/SimpleID.java
+++ src/main/java/tykkidream/keel/base/domain/SimpleID.java
@@ -4,12 +4,20 @@ public class SimpleID implements BaseID {
 	private Long id;
 
 	public SimpleID(Long id) {
-		this.id = id;
+		setId(id);
 	}
 
 	@Override
 	public Long longID() {
-		return this.id;
+		return getId();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 }
