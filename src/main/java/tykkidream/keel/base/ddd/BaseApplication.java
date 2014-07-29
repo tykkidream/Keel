b46930c6383ff888148e9fcedce57cae36a9811f
Tykkidream <Tykkidream@gmail.com>
2014-07-29 23:35:40 +0800

    最普通的业务方法（编辑、删除、查看）。

---
+++ src/main/java/tykkidream/keel/base/ddd/BaseApplication.java
@@ -0,0 +1,20 @@
+package tykkidream.keel.base.ddd;
+
+import java.util.List;
+import java.util.Map;
+
+import tykkidream.keel.base.Page;
+
+public interface BaseApplication<T,I> {
+	int saveOne(T t);
+	
+	int saveList(List<T> ts);
+	
+	int deleteOne(I i);
+	
+	int deleteList(List<I> is);
+	
+	T queryByKey(I i);
+	
+	List<T> queryByPage(Map<String, Object> params,Page page);
+}
